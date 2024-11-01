b8487113052ab8a0b63dfbbddfef32701d14f346
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-01 08:34:22 +0600

    adding Function related topic

---
+++ function.js
@@ -0,0 +1,18 @@
+/* function f1(){
+
+    console.log("working with function");
+}
+
+f1(); */
+
+function s1(P,Q){
+    const sum = P + Q ;
+    return sum ;
+}
+
+const Ans = s1(5,5);
+
+console.log("The Sum is = "+Ans);
+
+
+
---
+++ parameter.js
@@ -0,0 +1,13 @@
+function vatkha(){
+    console.log("hat dhuye asho");
+    console.log("vat kha");
+}
+
+vatkha();
+
+function r(X){
+    const  value = Math.sqrt(X);
+    return value;
+}
+const final = r(5);
+console.log(final);
